f007a9046d40ff3b18a7ee048b6d59cd9821d3ba
Mahmudul Hasan <121965285+mahmudulmr19@users.noreply.github.com>
2023-09-10 12:13:18 +0600

    Delete src directory

--- src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as Header } from "./Header/Header";
-export { default as Blog } from "./Blog/Blog";
-export { default as Card } from "./Card/Card";
-export { default as SpendTime } from "./SpendTime/SpendTime";
-export { default as Bookmark } from "./Bookmark/Bookmark";
-export { default as Faq } from "./Faq/Faq";
